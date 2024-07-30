aaf865c528bba1c8c7dbea8ff806e241792a2abc
Marq Roldan <hcmlroldan@gmail.com>
2024-07-30 12:32:20 +0800

    import specific

--- src/createContext.tsx
+++ src/createContext.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import {createContext as _createContext} from 'react';
 import type {DataContext, DefaultState} from './types';
 
 export function createContext<T = DefaultState>() {
-  return React.createContext<DataContext<T>>({
+  return _createContext<DataContext<T>>({
     getState: () => ({} as T),
     subscribe: () => () => {},
   });
